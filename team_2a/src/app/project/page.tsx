c010e43b7fd0ec3ed93b3b2a910ad699a1948eca
s2108tomoka <s2108.ochitomoka@gmail.com>
2024-09-04 15:59:13 +0900

    7 add project detail

---
+++ team_2a/src/app/project/page.tsx
@@ -0,0 +1,20 @@
+import styles from "../../../styles/project.module.css";
+
+export default function Home() {
+  return (
+    <>
+    <div className={styles.main}>
+    <div className={styles.project_title}>〇〇部署　〇〇〇〇プロジェクト</div>
+    <div className={styles.detail_box}>
+      <div className={styles.detail_content}>ここに使用技術や進捗など</div>
+    </div>
+    <div>
+      <div className={styles.subtitle}>関連書籍</div>
+      <div>
+        <a>ここに書籍一覧</a>
+      </div>
+    </div>
+    </div>
+    </>
+  );
+}
