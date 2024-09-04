a050d73a60173fcd5eea56c29cc22190b30d8b04
s2108tomoka <s2108.ochitomoka@gmail.com>
2024-09-04 17:08:01 +0900

    add list

--- team_2a/src/app/page.tsx
+++ team_2a/src/app/page.tsx
@@ -1,16 +1,30 @@
-import Image from "next/image";
-import style_main from "../../styles/project.module.css";
-import styles from "../../styles/Search_box.module.css";
-import Header from "../components/Header";
+import Image from 'next/image'
+import style_main from '../../styles/project.module.css'
+import styles from '../../styles/Search_box.module.css'
+import Header from '../components/Header'
+import List from '../components/List'
+
 export default function Home() {
-  return (
-    <>
-    <Header/>
-    <div className={style_main.main}>
-    <div className={styles.seach_space}>
-      <input placeholder="検索" className={styles.seach_box}></input>
-    </div>
-    </div>
-    </>
-  );
+    const titles = [
+        { titleString: 'プロジェクト名', width: 2 },
+        { titleString: '部署', width: 1 },
+        { titleString: '言語', width: 1 }
+    ]
+    const contents = [
+        ['Project A', 'Department A', 'JavaScript'],
+        ['Project B', 'Department B', 'TypeScript'],
+        ['Project C', 'Department C', 'Python']
+    ]
+    return (
+        <>
+            <Header />
+            <div className={style_main.main}>
+                <div className={styles.seach_space}>
+                    <input placeholder="検索" className={styles.seach_box}></input>
+                </div>
+            </div>
+            <div className={styles.space}></div>
+            <List titles={titles} contents={contents} />
+        </>
+    )
 }
