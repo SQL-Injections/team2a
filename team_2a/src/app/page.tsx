3b74a94110a28a05042368cb396f6db39b24e345
s2108tomoka <s2108.ochitomoka@gmail.com>
2024-09-04 16:12:25 +0900

    12 add Header

--- team_2a/src/app/page.tsx
+++ team_2a/src/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
+import style_main from "../../styles/project.module.css";
 import styles from "../../styles/Search_box.module.css";
-
+import Header from "../components/Header";
 export default function Home() {
   return (
     <>
-
+    <Header/>
+    <div className={style_main.main}>
     <div className={styles.seach_space}>
       <input placeholder="検索" className={styles.seach_box}></input>
     </div>
-
+    </div>
     </>
   );
 }
